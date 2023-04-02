a5fd01b8be5a0cdea6416f3765b221b9df73cd29
coolistkovbel <coolistkovbel@gmail.com>
2023-04-01 23:20:08 -0400

    getting ready for styles

--- src/App.js
+++ src/App.js
@@ -71,13 +71,60 @@ function App() {
             <h2>Getting started</h2>
           </header>
           <div className="setup-content">
-            <p>Well hopefully you choose yourself a group.</p>
-            <p></p>
+            <p>
+              Alrighty so you want to make sure you have metamask downloaded,
+              setup and create an account. Make sure you keep you keys safe and
+              sounds and dont loose it or else you will regret it. Anyways once
+              you gotten yourself situated. Congratz welcome to web3.
+            </p>
+            <p>
+              After that you will have options to choose different networks, I
+              would say hope on the <a>nervos network</a> but I also would
+              suggest staying on ethereum.
+            </p>
+            <p>
+              If you are looking to try and fund your accounts I strongly
+              suggest using{" "}
+              <a href="https://www.crypto.com/y8yjabr8my" target="_blank">
+                crypto.com
+              </a>
+            </p>
+            <p>
+              Once you got yourself with that also download the{" "}
+              <strong>defi wallet</strong>. That is where you want to store your
+              tokens. trust me. But fill free to buy and send your crypto where
+              ever. I would say to look into getting that crypto.com card. But
+              its up to you.
+            </p>
+            <p>
+              After that contact me letting me know you have done something or
+              there are some eager learners
+            </p>
           </div>
         </section>
 
         {/* Requirements */}
-        <section id="requirements"></section>
+        <section id="requirements">
+          <header>
+            <h2>You dont need much</h2>
+          </header>
+          <div className="requirement-content">
+            <p>
+              5 dollars a day will make a free man one day, as long as you have
+              piece of paper. Thats all you really need. I guess a pencil too so
+              you can write down your keys.
+            </p>
+            <p>
+              Oh and I guess i forgot, maybe a brain, cause you really dont want
+              to be like me and loose that paper that has the privkey to the
+              very first bitcoin wallet you created.
+            </p>
+            <p>
+              But I guess in like other terms, you have a phone, or computer, or
+              even a tablet.
+            </p>
+          </div>
+        </section>
 
         {/* contact me */}
         <section id="contact"></section>
